72843fccbde0ccd396afc331f1c8fa6fa3c58105
Sainath R <163316372+sainath-raja@users.noreply.github.com>
2025-08-17 20:37:12 +0530

    Time: 379 ms (92.24%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -1,4 +1,8 @@
-select a.id from Weather as a
-inner join Weather as b
-on a.recordDate = dateadd(day, 1, b.recordDate)
-where a.temperature > b.temperature
+with cte as(
+    select a.id, a.temperature as t1, b.temperature as t2,
+    lead(a.recordDate) over(order by a.recordDate) as ld from Weather as a
+    inner join Weather as b 
+    on a.recordDate = dateadd(day, 1,  b.recordDate)
+)
+select id from cte
+where t1 > t2
